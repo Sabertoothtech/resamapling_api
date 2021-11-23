80a382630c0ce6ee384b807e84190d9c0d86e4b9
SomyKamble <somykamble1001@gmail.com>
2021-11-23 12:15:41 +0530

    mean and std

--- api/views.py
+++ api/views.py
@@ -21,9 +21,10 @@ quaterly = pd.read_sql_table("calculated_resamplequaterly", con=engine)
 def sample_monthly(request,uid,pid):
     monthly_data =monthly[(monthly.user_id_id==float(uid)) & (monthly.portfolio_id_id == float(pid))]
     din = {}
+    monthly_data['cr']=monthly_data['cr'].round(4)
     din = monthly_data
-    din['mean'] = monthly_data['cr'].mean()
-    din['std'] = monthly_data['cr'].std()
+    din['mean'] = monthly_data['cr'].mean().round(4)
+    din['std'] = monthly_data['cr'].std().round(4)
     din=din.fillna('')
     print(din.fillna(''))
     return Response(din)
@@ -35,9 +36,11 @@ def sample_weekly(request,uid,pid):
     #& (monthly.portfolio_id_id == pid)
     #print(monthly)
     din = {}
+    weekly_data['cr']=weekly_data['cr'].round(4)
     din = weekly_data.drop_duplicates()
-    din['mean'] = weekly_data['cr'].mean()
-    din['std'] = weekly_data['cr'].std()
+    din['mean'] = weekly_data['cr'].mean().round(4)
+    din['std'] = weekly_data['cr'].std().round(4)
+
     din = din.fillna('')
     return Response(din)
 
@@ -51,9 +54,10 @@ def sample_quaterly(request,uid,pid):
     # mean_qaut=quaterly_data
     # print(type(mean_qaut))
     din={}
+    quaterly_data['cr']=quaterly_data['cr'].round(4)
     din=quaterly_data
-    din['mean']=quaterly_data['cr'].mean()
-    din['std']=quaterly_data['cr'].std()
+    din['mean']=quaterly_data['cr'].mean().round(4)
+    din['std']=quaterly_data['cr'].std().round(4)
     din = din.fillna('')
 
     return Response(din)
