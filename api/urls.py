e7130b4220b2fbda6439ac4240b92f7cb7be06c6
SomyKamble <somykamble1001@gmail.com>
2021-12-14 03:06:26 +0530

    mean and std

--- api/urls.py
+++ api/urls.py
@@ -20,6 +20,7 @@ from .views import *
 urlpatterns = [
    # path('admin/', admin.site.urls),
     path('monthly/<str:uid>/<str:pid>/<str:days>',sample_monthly),
+    path('resample/<str:uid>/<str:pid>/<str:days>',sample_monthly),
     path('quaterly/<str:uid>/<str:pid>/<str:days>',sample_quaterly),
     path('weekly/<str:uid>/<str:pid>/<str:days>',sample_weekly),
     path('daily/<str:uid>/<str:pid>/<str:days>',daily)
